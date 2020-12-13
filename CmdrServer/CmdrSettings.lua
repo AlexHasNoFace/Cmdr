950b0870504ebd0ef118d8b1e71194fc4c8f95e9
AlexHasNoFace <55563754+AlexHasNoFace@users.noreply.github.com>
2020-12-13 09:10:34 +0000

    Added settings entries for custom groups/ranks

--- CmdrServer/CmdrSettings.lua
+++ CmdrServer/CmdrSettings.lua
@@ -1,5 +1,7 @@
 settings = {}
 
 settings.Database = "[INSERT DB HERE]"
+settings.GroupId = 141878 -- Group ID for permissions
+settings.GroupRank = 255 -- Minimum group rank for permissions
 
 return settings
