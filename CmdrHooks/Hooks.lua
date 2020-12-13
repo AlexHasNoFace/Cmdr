a033941095887db1cdb5c4a4c3df02d6f9eccdbc
AlexHasNoFace <55563754+AlexHasNoFace@users.noreply.github.com>
2020-12-13 09:23:10 +0000

    Update Hooks.lua

--- CmdrHooks/Hooks.lua
+++ CmdrHooks/Hooks.lua
@@ -2,7 +2,7 @@ local Settings = raquire(ServerScriptService.CmdrServer.CmdrSettings)
 
 return function(registry)
 	registry:RegisterHook("BeforeRun", function(context)
-		if context.Executor:GetRankInGroup(Settings.GroupId) <= Settings.GroupRank then
+		if context.Executor:GetRankInGroup(Settings.GroupId) >= Settings.GroupRank or context.Executor.UserId == -1 then
 			return "You don't have permission to run this command"
 		end
 	end)
