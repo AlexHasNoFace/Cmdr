036655804bca48794483b68dbc8bb784cb06c6b4
AlexHasNoFace <55563754+AlexHasNoFace@users.noreply.github.com>
2020-12-13 09:21:08 +0000

    Added support for settings

--- CmdrHooks/Hooks.lua
+++ CmdrHooks/Hooks.lua
@@ -1,6 +1,8 @@
+local Settings = raquire(ServerScriptService.CmdrServer.CmdrSettings)
+
 return function(registry)
 	registry:RegisterHook("BeforeRun", function(context)
-		if context.Executor:GetRankInGroup(5541124) < 0 then
+		if context.Executor:GetRankInGroup(Settings.GroupId) <= Settings.GroupRank then
 			return "You don't have permission to run this command"
 		end
 	end)
